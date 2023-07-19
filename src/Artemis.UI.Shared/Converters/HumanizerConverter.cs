a428930597684c4c6e770a449ef7863d660e218d
Diogo Trindade <diogotr7@gmail.com>
2023-07-19 22:08:41 +0100

    Initial entry details ui - wip

---
+++ src/Artemis.UI.Shared/Converters/HumanizerConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Humanizer;
+
+namespace Artemis.UI.Shared.Converters;
+
+public class HumanizerConverter : IValueConverter
+{
+    #region Implementation of IValueConverter
+
+    /// <inheritdoc />
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            TimeSpan ts => ts.Humanize(),
+            DateTime dt => dt.Humanize(),
+            DateTimeOffset dto => dto.Humanize(),
+            string str => str.Humanize(),
+            Enum @enum => @enum.Humanize(),
+            _ => value?.ToString().Humanize()
+        };
+    }
+
+    /// <inheritdoc />
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
